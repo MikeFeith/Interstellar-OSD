f4b1a3f5aef3296f2405183f012e0389d61c9f2d
Mike Feith <mike.feith@gmail.com>
2024-10-03 08:40:10 +0200

    2

---
+++ Emergis/starturl.ps1
@@ -0,0 +1,8 @@
+#start-osd cloud
+
+
+
+Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSActivation Volume -OSLanguage nl-nl -OSEdition Enterprise
+#copy the unattend xml from the usb  to C:\Windows\panther\unattend\unattend.xml
+Copy-Item -Path '' -Destination 'C:\Windows\panther\unattend\unattend.xml' -Force
+
