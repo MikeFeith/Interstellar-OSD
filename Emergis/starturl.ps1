5e13dc1c9e53eb790aad64dee80f79540755a16c
Mike Feith <mike.feith@gmail.com>
2024-10-10 11:36:56 +0200

    Update starturl.ps1

--- Emergis/starturl.ps1
+++ Emergis/starturl.ps1
@@ -1,19 +1,22 @@
 #start-osd cloud
 #used command: Edit-OSDCloudWinPE -Add7Zip -PSModuleInstall AzureAD, WindowsAutopilotIntune -Starturl "https://raw.githubusercontent.com/MikeFeith/Interstellar-OSD/refs/heads/main/Emergis/starturl.ps1" -wallpaper "C:\temp\emergis.jpg" -clouddriver HP,IntelNet
 
-#Variables to define the Windows OS / Edition etc to be applied during OSDCloud
+#=======================================================================
+#   OSDCLOUD Definitions
+#=======================================================================
 $Product = (Get-MyComputerProduct)
 $Model = (Get-MyComputerModel)
 $Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
 $OSVersion = 'Windows 11' #Used to Determine Driver Pack
-$OSReleaseID = '24H2' #Used to Determine Driver Pack
-$OSName = 'Windows 11 24H2 x64'
+$OSReleaseID = '23H2' #Used to Determine Driver Pack
+$OSName = 'Windows 11 23H2 x64'
 $OSEdition = 'Enterprise'
 $OSActivation = 'Volume'
-$OSLanguage = 'en-us'
+$OSLanguage = 'nl-nl'
 
-
-#Set OSDCloud Vars
+#=======================================================================
+#   OSDCLOUD VARS
+#=======================================================================
 $Global:MyOSDCloud = [ordered]@{
     Restart = [bool]$False
     RecoveryPartition = [bool]$true
@@ -29,14 +32,15 @@ $Global:MyOSDCloud = [ordered]@{
     CheckSHA1 = [bool]$true
 }
 
-#Used to Determine Driver Pack
+#=======================================================================
+#   Specific Driver Pack
+#=======================================================================
 $DriverPack = Get-OSDCloudDriverPack -Product $Product -OSVersion $OSVersion -OSReleaseID $OSReleaseID
 
 if ($DriverPack){
     $Global:MyOSDCloud.DriverPackName = $DriverPack.Name
 }
-$UseHPIA = $false
-$UseMSDriverPack = $false
+$UseHPIA = $true
 if ($Manufacturer -match "HP" -and $UseHPIA -eq $true) {
     #$Global:MyOSDCloud.DevMode = [bool]$True
     $Global:MyOSDCloud.HPTPMUpdate = [bool]$True
@@ -44,37 +48,21 @@ if ($Manufacturer -match "HP" -and $UseHPIA -eq $true) {
     $Global:MyOSDCloud.HPBIOSUpdate = [bool]$true
     $Global:MyOSDCloud.HPCMSLDriverPackLatest = [bool]$true
 }
-if ($Manufacturer -match "Microsoft" -and $UseMSDriverPack -eq $true) {
-    #Updating Microsoft Surface Driver catalog
-    Write-Host "Updating Microsoft Surface Driver catalog"
-    Invoke-RestMethod "https://raw.githubusercontent.com/chield/OSDCloud/main/Update-OSDCloudSurfaceDriverCatalogJustInTime.ps1" | Invoke-Expression
-    Update-OSDCloudSurfaceDriverCatalogJustInTime.ps1 -UpdateDriverPacksJson
-}
 
-#write variables to console
+#=======================================================================
+#   Write OSDCloud VARS to Console
+#=======================================================================
 Write-Output $Global:MyOSDCloud
 
-#download answer file from github https://raw.githubusercontent.com/MikeFeith/Interstellar-OSD/refs/heads/main/Emergis/Autounattend.xml -outfile "C:\Windows\panther\unattend\unattend.xml"
-$UnattendXml = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/MikeFeith/Interstellar-OSD/refs/heads/main/Emergis/Autounattend.xml" -UseBasicParsing | Select-Object -ExpandProperty Content
-
-$PantherUnattendPath = 'C:\Windows\Panther\Unattend\'
-if (-NOT (Test-Path $PantherUnattendPath)) {
-    New-Item -Path $PantherUnattendPath -ItemType Directory -Force | Out-Null
-}
-$SpecUnattendPath = Join-Path $PantherUnattendPath 'unattend.xml'
-
-
-Write-Host -ForegroundColor Cyan "Set Unattend.xml at $SpecUnattendPath"
-$UnattendXml | Out-File -FilePath $SpecUnattendPath -Encoding utf8
-
-Write-Host -ForegroundColor Cyan 'Use-WindowsUnattend'
-Use-WindowsUnattend -Path 'C:\' -UnattendPath $SpecUnattendPath -Verbose
-
+#=======================================================================
+#   Start OSDCloud installation
+#=======================================================================
 Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
 
-#////////////////////////////////////////////////////////////////////////
+#=======================================================================
+#   OSDCloud Specialize
+#=======================================================================
 Set-OSDCloudUnattendSpecialize
-
 
 $UnattendXml = @'
 <?xml version='1.0' encoding='utf-8'?>
@@ -88,28 +76,26 @@ $UnattendXml = @'
                     <Path>Powershell -ExecutionPolicy Bypass -Command Invoke-OSDSpecialize -Verbose</Path>
                 </RunSynchronousCommand>
                 <RunSynchronousCommand wcm:action="add">
-                  <Order>2</Order>
-                  <Description>Pop up to confirm unattend is working</Description>
-                  <Path>powershell.exe -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Unattend is working')"</Path>
+                    <Order>2</Order>
+                    <Description>Add to autopilot</Description>
+                    <Path>powershell.exe -ExecutionPolicy Bypass -file "C:\Windows\Panther\fblocalscripts\Autopilot\Menu.ps1" -Verbose</Path>
                 </RunSynchronousCommand>
                 <RunSynchronousCommand wcm:action="add">
-                  <Order>3</Order>
-                  <Description>Add to autopilot</Description>
-                  <Path>powershell.exe -ExecutionPolicy Bypass -file "C:\Windows\Panther\fblocalscripts\Autopilot\Menu.ps1"</Path>
+                    <Order>3</Order>
+                    <Description>Remove OSDCloud Temp Files</Description>
+                    <Path>Powershell -ExecutionPolicy Bypass -Command Remove-Item -Path C:\OSDCloud -Recurse</Path>
                 </RunSynchronousCommand>
                 <RunSynchronousCommand wcm:action="add">
                     <Order>4</Order>
-                    <Description>Remove OSDCloud Temp Files</Description>
-                    <Path>Powershell -ExecutionPolicy Bypass -Command Remove-Item -Path C:\OSDCloud -Recurse</Path>
-                </RunSynchronousCommand>          
+                    <Description>Remove localscript Files</Description>
+                    <Path>Powershell -ExecutionPolicy Bypass -Command Remove-Item -Path C:\Windows\Panther\fblocalscripts -Recurse</Path>
+                </RunSynchronousCommand>             
             </RunSynchronous>
         </component>
     </settings>    
 </unattend>
 '@
-#================================================================================================
-#   Set Unattend.xml
-#================================================================================================
+
 $PantherUnattendPath = 'C:\Windows\Panther'
 if (-NOT (Test-Path $PantherUnattendPath)) {
     New-Item -Path $PantherUnattendPath -ItemType Directory -Force | Out-Null
@@ -122,27 +108,28 @@ Invoke-Exe reg load HKLM\TempSYSTEM "C:\Windows\System32\Config\SYSTEM"
 Invoke-Exe reg add HKLM\TempSYSTEM\Setup /v UnattendFile /d "C:\Windows\Panther\Invoke-OSDSpecialize.xml" /f
 Invoke-Exe reg unload HKLM\TempSYSTEM
 
-#////////////////////////////////////////////////////////////////////////
-
-
-
-#copy the local scripts to the panther folder
+#=======================================================================
+#   OSDCLOUD Specialize Scripts and modules
+#=======================================================================
 $localscriptsosdfolder = "D:\fblocalscripts"
 $localscriptfolderPath = "C:\Windows\Panther\fblocalscripts"
 if (-NOT (Test-Path "$localscriptfolderPath")) {
     New-Item -Path "$localscriptfolderPath" -ItemType Directory -Force | Out-Null
 }
 Copy-Item -Path $localscriptsosdfolder\* -Destination $localscriptfolderPath\ -Recurse -Force
-Install-Script -Name Get-WindowsAutoPilotInfo -Force -AcceptLicense -Scope AllUsers
 install-module -Name AzureAD -Force -AcceptLicense -Scope AllUsers -SkipPublisherCheck
 install-module -Name WindowsAutopilotIntune -Force -AcceptLicense -Scope AllUsers -SkipPublisherCheck
+
+#=======================================================================
+#   DEVELOPER USER CONFIRMATION TO REBOOT
+#=======================================================================
 Start-Sleep -Seconds 15
 #wait for user input to continue
 Write-Host -ForegroundColor Yellow "Press any key to reboot the device"
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
 #=======================================================================
-#   Restart-Computer
+#   REBOOT DEVICE
 #=======================================================================
 Write-Host  -ForegroundColor Green "Restarting now!"
 Restart-Computer -Force
